930468eb3e05800d26f49c9239cbc1745ae027c1
Backplate Orbit <backplateorbit@gmail.com>
2022-05-01 18:14:41 +0100

    doc: added docstrings to client.py

--- horny_police/client.py
+++ horny_police/client.py
@@ -9,9 +9,14 @@ from horny_police import detection
 from horny_police import logging
 
 class HornyPoliceClient(discord.Client):
+    """Class encompassing Discord Bot functionality."""
+
     logger = logging.get_logger()
+
+    # path to image to send when moderation complete and replying to user.
     REPLY_IMAGE = pathlib.Path("/workspaces/horny-police/reply_image.png")
 
+    # raised in the event that a TEMP_SAVE_DIRECTORY has not been configured.
     if not os.getenv("TEMP_SAVE_DIRECTORY"):
         logger.error(
             "temp directory for saving files not configured, "
@@ -22,19 +27,29 @@ class HornyPoliceClient(discord.Client):
     temporary_directory = pathlib.Path(os.getenv("TEMP_SAVE_DIRECTORY"))  # type: ignore
 
     async def on_ready(self) -> None:
+        """Function run upon the bot first logging in.
+        """
         HornyPoliceClient.logger.info("bot ready for tasking...")
     
     async def on_message(self, message: discord.Message) -> None:
+        """Event handler for messages that acts as entrypoint for moderation.
+
+        Args:
+            message (discord.Message): Message to be checked for moderation.
+        """
         attachments = message.attachments
 
+        # if message has no attachments, nothing to moderate (...yet!)
         if not attachments:
             return
 
+        # if the bot itself posted the image,ignore it.
         if message.author.id == self.user.id: # type: ignore
             return
 
         HornyPoliceClient.logger.info("detected message with attachments, scanning...")
 
+        # runs through attachments, returning after first moderation.
         for attachment in attachments:
             
             HornyPoliceClient.logger.info(
@@ -66,6 +81,7 @@ class HornyPoliceClient(discord.Client):
             result = detection.detect_nsfw(attachment_file_path)
 
             # through testing, this feels about right as a threshold.
+            # TODO: add this as configurable env variable.
             if result["unsafe"] >= 0.40:
                 HornyPoliceClient.logger.info(
                     f"file {attachment.filename} likely nsfw, deleting"
